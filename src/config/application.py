b6a2e2dbfe7fa46c5c7a0ba8a03f13cbe2eb18dd
Alvaro Tomas Rosas <tom@rookmotion.com>
2023-09-29 19:15:18 -0600

    Feat: Initial project config * Helper was added the errors to endpoints * Initial configuration was added of  linter flake8 * Configuration was added  of the logs * Tndpoint was added of the test

---
+++ src/config/application.py
@@ -0,0 +1,21 @@
+import os
+
+from logging.config import dictConfig
+from src.config.logger import CONFIG
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    JSON_SORT_KEYS = False
+
+    dictConfig(CONFIG)
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    TESTING = True
+
+
+config = {
+    'development': DevelopmentConfig
+    }
