62a020062a3ca54d7d87d341228ad52d83c16525
Alvaro Tomas Rosas <tom@rookmotion.com>
2023-10-04 19:25:43 -0600

    Feat: Readme was updated * The instructions of the development was added

--- src/config/application.py
+++ src/config/application.py
@@ -1,15 +1,13 @@
 import os
 
-from pathlib import Path
 from logging.config import dictConfig
 from src.config.logger import CONFIG
 
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY', None)
     JSON_SORT_KEYS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     dictConfig(CONFIG)
@@ -18,19 +16,16 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class TestingConfig(Config):
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_DIR)}/database.db'
 
 
 class ProductionConfig(Config):
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 config = {
