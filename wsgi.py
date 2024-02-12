aa375fc5d1f8e78482599a05417e734015781ca4
Alvaro Tomas Rosas <tom@rookmotion.com>
2024-02-11 21:08:17 -0600

    feat: docker implemented

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,5 @@ from src import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(ssl_context='adhoc')
+    # Remove ssl_context='adhoc' when make we deploy and change for host='0.0.0.0'
+    app.run(host='0.0.0.0')
